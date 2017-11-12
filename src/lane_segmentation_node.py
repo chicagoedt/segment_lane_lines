f4bdb30cac0c0a57c5e076c693212c6cf81c687c
Jeff <JeffKo427@gmail.com>
2017-11-12 12:43:50 -0600

    renamed variables to make them private and restructured program flow

--- src/lane_segmentation_node.py
+++ src/lane_segmentation_node.py
@@ -9,13 +9,14 @@ Todo:
     * Implement homographic transform methods.
     * Refactoring and documentation. (clean up variable names!)
     * Rewrite model to augment predictions using predictions from previous frames.
+    * Build fully-convolutional model dynamically to accomodate images of different sizes.
 """
 
 import sys
 import time
 from keras.models import load_model
 import rospy
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, PointCloud2
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 import tensorflow as tf
@@ -23,68 +24,77 @@ import cv2
 
 
 class LaneSegmentationNode:
-    """Class containing all attributes of lane_segmentation_node.
-
-    If the class has public attributes, they may be documented here
-    in an ``Attributes`` section and follow the same formatting as a
-    function's ``Args`` section. Alternatively, attributes may be documented
-    inline with the attribute's declaration (see __init__ method below).
-
-    Properties created with the ``@property`` decorator should be documented
-    in the property's getter method.
+    """Class providing all functionality of lane_segmentation_node.
 
     Attributes:
-        attr1 (str): Description of `attr1`.
-        attr2 (:obj:`int`, optional): Description of `attr2`.
+        __model (keras.models.Model): The Keras Model object used to segment the
+            numpy array containing the image data.
+        __bridge (cv_bridge.CvBridge): The default CvBridge object.
+        __graph (tf.Graph): The TensorFlow Graph for the model.
+        __image_sub(rospy.Subscriber): Subscriber object which invokes the
+            segmentation and transormation as its callback.
+        __image_pub (rospy.Publisher):
+        __cloud_pub (rospy.Publisher):
 
     """
     def __init__(self):
-        """LaneSegmentationNode constructor.
+        """LaneSegmentationNode constructor."""
+        self.__model = load_model(sys.argv[1])
+        self.__graph = tf.get_default_graph()
+        self.__bridge = CvBridge()
+        self.__image_sub = rospy.Subscriber("/stereo_camera/left/image_color", Image, self.callback, queue_size=1, buff_size=100000000)
+        self.__image_pub = rospy.Publisher("/stereo_camera/left/lanes", Image, queue_size=1)
+        self.__cloud_pub = rospy.Publisher("/topic/name/here", PointCloud2, queue_size=1)
 
-        The __init__ method may be documented in either the class level
-        docstring, or as a docstring on the __init__ method itself.
-
-        Either form is acceptable, but the two should not be mixed. Choose one
-        convention to document the __init__ method and be consistent with it.
-
-        Note:
-            Do not include the `self` parameter in the ``Args`` section.
-
-        Args:
-            param1 (str): Description of `param1`.
-            param2 (:obj:`int`, optional): Description of `param2`. Multiple
-                lines are supported.
-            param3 (:obj:`list` of :obj:`str`): Description of `param3`.
-
-        """
-        self.model = load_model(sys.argv[1])
-        self.graph = tf.get_default_graph()
-        self.image_pub = rospy.Publisher("/stereo_camera/left/lanes", Image, queue_size=1)
-        self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("/stereo_camera/left/image_color", Image, self.callback, queue_size=1, buff_size=100000000)
-
-    def segment_and_publish(self, data):
-        """Class methods are similar to regular functions.
-
-        Note:
-            Do not include the `self` parameter in the ``Args`` section.
+    def __ros_to_numpy(self, data):
+        """Extracts image data from Image message.
 
         Args:
-            param1: The first parameter.
-            param2: The second parameter.
+            data (sensor_msgs/Image): The ROS Image message, exactly as passed
+                by the subscriber to its callback.
 
         Returns:
-            True if successful, False otherwise.
+            The image, as a NumPy array.
+
+        """
+        try:
+            raw_img = self.__bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as err:
+            print(err)
+
+        return raw_img
+
+    def __numpy_to_rox(self, img):
+        """Builds a Image message from a NumPy array.
+
+        Args:
+            img (np.array): A NumPy array containing the RGB image data.
+
+        Returns:
+            A sensor_msgs/Image containing the image data.
+
+        """
+        try:
+            data = self.__bridge.cv2_to_imgmsg(img, "rgb8")
+        except CvBridgeError as err:
+            print(err)
+
+        return data
+
+    def __segment_and_publish(self, img):
+        """Runs the segmentation model on the
+
+        Args:
+            data (sensor_msgs/Image): The ROS Image message, exactly as passed
+                by the subscriber to its callback.
+
+        Returns:
+            A NumPy array representing the segmented image.
 
         """
         start = time.clock()
-        # sensor_msgs/Image -> NumPy array
-        try:
-            raw_img = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        except CvBridgeError as e:
-            print(e)
         # Preprocess input.
-        model_input = np.array([raw_img]).astype('float32') / 255
+        model_input = np.array([img]).astype('float32') / 255
         # Run the semantic segmentation model.
         with self.graph.as_default():
             model_output = self.model.predict(model_input)[0] * 255
@@ -102,7 +112,7 @@ class LaneSegmentationNode:
 
         return lane_lines
 
-    def transform_and_publish(self, segmented_image):
+    def __transform_and_publish(self, segmented_image):
         """Class methods are similar to regular functions.
 
         Note:
@@ -118,7 +128,7 @@ class LaneSegmentationNode:
         """
         pass
 
-    def callback(self, data):
+    def __callback(self, data):
         """Class methods are similar to regular functions.
 
         Note:
